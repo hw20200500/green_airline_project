8b9bfe4eeb42b61f8495f671d4966daba1df0144
master <kmg1151@naver.com>
2023-06-07 21:21:13 +0900

    수정

--- src/main/java/com/green/airline/controller/PaymentController.java
+++ src/main/java/com/green/airline/controller/PaymentController.java
@@ -172,6 +172,9 @@ public class PaymentController {
 		
 		model.addAttribute("ticketList", ticketList);
 		
+		User principal = (User) session.getAttribute(Define.PRINCIPAL);
+		System.out.println(principal.getId());
+		System.out.println();
 		return "/ticket/paymentSuccess";
 	}
 	
