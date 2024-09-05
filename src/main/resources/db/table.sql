99a382dee3d8fef33f783b5edde1479d360fc78c
0cupid <144975721+0cupid@users.noreply.github.com>
2024-09-05 12:50:06 +0900

    오류 해결

--- src/main/resources/db/table.sql
+++ src/main/resources/db/table.sql
@@ -334,7 +334,7 @@ CREATE TABLE gifticon_tb(
 
 -- 기프티콘 환불
 CREATE TABLE gifticon_revoke_tb(
-<<<<<<< HEAD
+-- <<<<<<< HEAD
    id int PRIMARY KEY auto_increment,
    revoke_date DATE DEFAULT (CURRENT_DATE),
    gifticon_id INT NOT NULL,
@@ -342,17 +342,8 @@ CREATE TABLE gifticon_revoke_tb(
    );
    
 -- 마일리지
-CREATE TABLE mileage_tb(
-=======
-	id int PRIMARY KEY auto_increment,
-	revoke_date DATE DEFAULT (CURRENT_DATE),
-	amount Bigint not null,
-	brand VARCHAR(20) not null,
-	name VARCHAR(50) not null
-	);
--- 마일리지
 create table mileage_tb(
->>>>>>> feature/myPage2
+-- >>>>>>> feature/myPage2
 id int primary key auto_increment,
 mileage_date DATE NOT NULL,
 use_mileage BIGINT,
